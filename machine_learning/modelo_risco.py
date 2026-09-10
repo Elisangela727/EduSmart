1463ac9ff11e761f271398d553a2a6ca9c3a8152
Elisângela Aparecida Alves dos Santos <elisangela.santos1986@outlook.com>
2026-09-10 20:36:14 -0300

    feat: cria primeiro modelo de risco acadêmico

---
+++ machine_learning/modelo_risco.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+dados = pd.read_csv("machine_learning/dados_teste.csv")
+X = dados[["media_notas", "frequencia"]]
+y = dados["risco"]
+
+modelo = DecisionTreeClassifier()
+modelo.fit(X, y)
+novo_aluno = pd.DataFrame([[4.0, 65]], columns=["media_notas", "frequencia"])
+resultado = modelo.predict(novo_aluno)
+print(resultado[0])     
