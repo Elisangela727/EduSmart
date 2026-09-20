d00c57e569c6625d0f96b1c82e4aaf2f8d541c99
Guilherme Almeida <guilhermesalmeida1212@gmail.com>
2026-09-20 15:09:43 -0300

    fix: adapta servico de machine learning para deploy

--- machine_learning/app.py
+++ machine_learning/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, jsonify, request
 
 from modelo_risco import prever_risco
@@ -56,8 +58,10 @@ def predict():
 
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+
     app.run(
-        host="127.0.0.1",
-        port=5000,
-        debug=True
+        host="0.0.0.0",
+        port=port,
+        debug=False
     )
